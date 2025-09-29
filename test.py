0537caacf81c8e2ca4f640dbb06e17e94337fe7a
Saurabh Datta <datta@zigzag.is>
2025-09-29 23:07:18 +0200

    adding gpio capabilities

--- test.py
+++
@@ -1,8 +0,0 @@
-from loguru import logger
-
-try:
-    import RPi.GPIO as GPIO
-    IS_PI = True
-except (RuntimeError, ModuleNotFoundError):
-    IS_PI = False
-    logger.warning("RPi.GPIO library not found. GPIO functionality will be disabled.")
