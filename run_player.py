650847ad35911a47850f4c4b6b00b562d3aa1012
Saurabh Datta <datta@zigzag.is>
2026-01-05 02:31:18 +0100

    rebase

--- run_player.py
+++ run_player.py
@@ -85,9 +85,6 @@ def keep_audio_alive(stop_event: threading.Event):
             preload=True  # Use the new feature
         )
         silent_player.play()
-        time.sleep(1)
-        silent_player.stop()
-        time.sleep(5)
         
         # This thread will now simply wait until the main app signals it to stop.
         stop_event.wait()
